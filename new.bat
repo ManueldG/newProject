73fd11632f957451ef2d218c790b91251f5b4f29
Manuel della Gala <74384683+ManueldG@users.noreply.github.com>
2021-06-11 10:29:41 +0200

    Update new.bat

--- new.bat
+++ new.bat
@@ -5,8 +5,16 @@ cls
 echo preparazione del progetto %1
 
 call composer create-project --prefer-dist laravel/laravel:^7.0 %1 
+@color 0a
 cd %1
+@color 0a
 call   npm install
+@color 0a
 call  composer require barryvdh/laravel-debugbar --dev
+@color 0a
+call  composer remove composer remove fzaninotto/faker
+@color 0a
+call  composer require fakerphp/faker
+@color 0a
 
 echo Ho fatto puoi iniziare 
